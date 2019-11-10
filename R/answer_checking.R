4e65f4af08fdcf363e90765d94f86d52caa88bf4
Lincoln <lincoln@colling.net.nz>
2019-11-10 21:00:19 +0000

    updated

--- R/answer_checking.R
+++ R/answer_checking.R
@@ -1,34 +1,36 @@
 #' @export
 task1 <- function(A,B,C){
   if((A + B + C) == 22){
-    cat("Correct! Answer Code 1 / 1 = 'jsu2'")
+    cat("Correct! Answer Code 1 / 1 = 'jsu2'\n")
   } else
-  {cat("Incorrect!")}
+  {cat("Incorrect!\n")
+}
 }
 
 #' @export
 task2 <- function(called,my_var){
   if(called == my_var && my_var == 11){
-    cat("Correct! Answer Code 1 / 2 = 'kliw")
-  } else
-    cat("Incorrect!")
+    cat("Correct! Answer Code 1 / 2 = 'kliw'\n")
+  } else {
+    cat("Incorrect!\n")
+  }
 }
 
 #' @export
 task3 <- function(collect,pineapple){
   if(sum(collect == c("pineapple","pizza","cheese")) == 3 && pineapple == "pizza"){
-    cat("Correct! Answer Code 1 / 3 = 'wr2a")
+    cat("Correct! Answer Code 1 / 3 = 'wr2a'\n")
   } else {
-    cat("Incorrect!")
+    cat("Incorrect!\n")
   }
 }
 
 #' @export
 task4 <- function(score_gender_tib_filtered){
   if(identical(score_gender_tib_filtered,score_gender_tib %>% filter(score > 2.5))){
-    cat("Correct! Answer Code 2 / 1 = 'ety2")
+    cat("Correct! Answer Code 2 / 1 = 'ety2'\n")
   } else {
-    cat("Incorrect")
+    cat("Incorrect\n")
   }
 
 }
@@ -36,18 +38,18 @@ task4 <- function(score_gender_tib_filtered){
 #' @export
 task5 <- function(DE23){
   if(identical(DE23, stroop_tib %>% filter(id == "DE23"))){
-    cat("Correct! Answer Code 2 / 2 = 'iowl'")
+    cat("Correct! Answer Code 2 / 2 = 'iowl'\n")
   } else {
-    cat("Incorrect")
+    cat("Incorrect\n")
   }
 }
 
 #' @export
 task6 <- function(exp1_tib,exp1_task1_tib){
   if(identical(exp1_task1_tib,exp1_tib %>% select(-task2))){
-    cat("Correct! Answer Code 2 / 3 = '89js'")
+    cat("Correct! Answer Code 2 / 3 = '89js'\n")
   } else {
-    cat("Incorrect")
+    cat("Incorrect\n")
   }
 }
 
@@ -61,8 +63,8 @@ task10 <- function(stroop_tib_outliers,outlier_ids,unique_outlier_ids){
   part3 = identical(unique_outlier_ids,unique(outlier_ids))
 
   if(sum(part1,part2,part3) == 3){
-    cat("Correct!")
+    cat("Correct!\n")
   } else {
-    cat("Incorrect")
+    cat("Incorrect\n")
   }
 }
