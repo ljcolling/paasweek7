bbe88fd2541e8b2727bf99c4ed079887c2847cb4
Lincoln <lincoln@colling.net.nz>
2019-11-10 20:53:53 +0000

    updated

--- R/answer_checking.R
+++ R/answer_checking.R
@@ -27,13 +27,18 @@ task3 <- function(collect,pineapple){
 task4 <- function(score_gender_tib_filtered){
   if(identical(score_gender_tib_filtered,score_gender_tib %>% filter(score > 2.5))){
     cat("Correct! Answer Code 2 / 1 = 'ety2")
+  } else {
+    cat("Incorrect")
   }
+
 }
 
 #' @export
 task5 <- function(DE23){
   if(identical(DE23, stroop_tib %>% filter(id == "DE23"))){
     cat("Correct! Answer Code 2 / 2 = 'iowl'")
+  } else {
+    cat("Incorrect")
   }
 }
 
@@ -41,6 +46,8 @@ task5 <- function(DE23){
 task6 <- function(exp1_tib,exp1_task1_tib){
   if(identical(exp1_task1_tib,exp1_tib %>% select(-task2))){
     cat("Correct! Answer Code 2 / 3 = '89js'")
+  } else {
+    cat("Incorrect")
   }
 }
 
@@ -55,5 +62,7 @@ task10 <- function(stroop_tib_outliers,outlier_ids,unique_outlier_ids){
 
   if(sum(part1,part2,part3) == 3){
     cat("Correct!")
+  } else {
+    cat("Incorrect")
   }
 }
